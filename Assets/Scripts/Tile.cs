e872a8087922bdc5c0cc5544f4461f5a27001c35
Duncak46 <123555933+Duncak46@users.noreply.github.com>
2024-04-16 08:35:21 +0200

    dsfs
    fsdfds

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    int kolikstoji = 5;
+    public ScriptableObjects scriptableObject;
+
+    int kolikStoji1;
+    int kolikStoji2;
+    public static string kteraBudova;
+
+    void NastaveniHodnot()
+    {
+        //Koule
+        hodnoty_do_seznamu firstItem = scriptableObject.seznamPolozek[0];
+        kolikStoji1 = firstItem.kolik_stoji_gold;
+
+        //Dvojèata
+        hodnoty_do_seznamu secondItem = scriptableObject.seznamPolozek[1];
+        kolikStoji2 =secondItem.kolik_stoji_gold;
+    }
+
     public TileData TileData;
 
     public GameObject buildingPrefab;
     private int koliktostoji = 5;
     internal void Build(GameObject prefab)
     {
-        // Vytvoøí prefab na pozici støedu políèka
+
         Vector3 centerPosition = transform.position + new Vector3(1f, 0.2f, 1f);
         var bld = Instantiate(prefab, centerPosition, Quaternion.identity, transform);
         TileData.isOccupied = true;
@@ -41,5 +57,19 @@ public class Tile : MonoBehaviour
                 Resorces.gold += koliktostoji * 2;
             }
         }
+    }
+    void Update()
+    {
+        Debug.Log(kteraBudova);
+    }
+    public GameObject Dvojcata;
+    public GameObject Koule;
+    public void SetDvojcata()
+    {
+        buildingPrefab = Dvojcata;
+    }
+    public void SetKoule()
+    {
+        buildingPrefab = Koule;
     }
 }
